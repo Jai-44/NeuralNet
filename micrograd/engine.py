0d7ace7e0519cd62586983284859f178e79bde9c
Jai <kasinajaishankar05@gmail.com>
2025-08-17 14:43:14 +0530

    Add _inputs and _op  fields

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,51 +1,58 @@
 
 class Value:
-
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = float(data)
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
-        return f"Value => data = {self.data}"
+        out = f"Value => data = {self.data}"
+        return out
+    # __repr__ is a dunder method which provides a readable string reprsention.
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
     __radd__ = __add__
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
     __rmul__ = __mul__
 
+   # Addition and multiplication are commutative, so we can alias __radd__ = __add__ and __rmul__ = __mul__.
+    # Subtraction and division are not commutative, so __rsub__ and __rtruediv__ need their own implementations.
+
     def __neg__(self):
-        return Value(-self.data)
+        out = Value(-self.data, (self,), 'neg')
+        return out
 
     def __sub__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(self.data - other.data)
+        out = Value(self.data - other.data, (self, other), '-')
+        return out
 
     def __rsub__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(other.data - self.data)
+        out = Value(other.data - self.data, (other, self), '-')
+        return out
 
     def __truediv__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(self.data / other.data)
+        out = Value(self.data / other.data, (self, other), '/')
+        return out
 
     def __rtruediv__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        return Value(other.data / self.data)
+        out = Value(other.data / self.data, (other, self), '/')
+        return out
 
     def __pow__(self, other):
         if isinstance(other, Value):
-            return Value(self.data ** other.data)
-        return Value(self.data ** float(other))
-
-
-a = Value(2)
-b = Value(3)
-c = a + b
-d = Value(-7)
-e = c - d
-
-print(e)
+            out = Value(self.data ** other.data, (self, other), '**')
+        else:
+            out = Value(self.data ** float(other),
+                        (self,), f'**{float(other)}')
+        return out
